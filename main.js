5a2e819d54c605a27f6e0cf89b685a700b120252
User <user@example.com>
2026-08-14 18:56:47 +0530

    fix: add timeout to pkexec so linux gui doesn't hang indefinitely

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function checkPermissionsAndStart(win) {
             const { exec } = require('child_process');
             const setupCommand = `pkexec bash -c "echo 'KERNEL==\\"uinput\\", MODE=\\"0666\\"' > /etc/udev/rules.d/99-psp-uinput.rules && udevadm control --reload-rules && udevadm trigger && chmod 666 /dev/uinput"`;
             
-            exec(setupCommand, (error) => {
+            exec(setupCommand, { timeout: 10000 }, (error) => {
                 if (error) {
                     win.webContents.executeJavaScript(`
                         document.getElementById('status').innerText = 'Status: Error (Permissions)';
